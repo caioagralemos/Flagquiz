6a831d3f0006d1e910fd15f7ece3722697dbd03d
Caio <caioagralemos@live.com>
2024-06-26 20:13:10 -0300

    Initial Commit

---
+++ Flagquiz/FlagquizApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagquizApp.swift
+//  Flagquiz
+//
+//  Created by Caio on 26/06/24.
+//
+
+import SwiftUI
+
+@main
+struct FlagquizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
